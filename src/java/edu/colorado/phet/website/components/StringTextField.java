b4bf5d5f3c3a904983710e0f2d36211df455143b
Jonathan Olson <olsonsjc@gmail.com>
2010-12-11 17:43:41 -0800

    website: removed todo

--- src/java/edu/colorado/phet/website/components/StringTextField.java
+++ src/java/edu/colorado/phet/website/components/StringTextField.java
@@ -3,7 +3,6 @@ package edu.colorado.phet.website.components;
 import org.apache.wicket.markup.html.form.TextField;
 import org.apache.wicket.model.IModel;
 
-//TODO: Get rid of this class and use generics instead
 public class StringTextField extends TextField<String> {
     public StringTextField( String id, IModel<String> object ) {
         super( id, object );
