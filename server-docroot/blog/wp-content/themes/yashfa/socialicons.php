9305d33200da145b2c6f4ddbf8209033e7ed9590
Jonathan Olson <olsonsjc@gmail.com>
2011-01-19 04:16:00 -0800

    website: changing alt attributes for blog social icons to avoid keyword cannibalization

--- server-docroot/blog/wp-content/themes/yashfa/socialicons.php
+++ server-docroot/blog/wp-content/themes/yashfa/socialicons.php
@@ -4,7 +4,7 @@
        style="padding: 0; margin: 0; text-decoration: none;" target="_blank"
        title="Share this on Facebook">
 
-        <img src="/files/icons/social/16/facebook.png" alt="Social Media Icon"
+        <img src="/files/icons/social/16/facebook.png" alt="Share this on Facebook"
              style="border: none;"/>
     </a>
 </div>
@@ -12,7 +12,7 @@
     <a href="https://twitter.com/share?url=<?php echo urlencode( get_permalink( ) ); ?>&amp;text=<?php echo urlencode( get_the_title( ) ); ?>"
        style="padding: 0; margin: 0; text-decoration: none;" target="_blank"
        title="Share this on Twitter">
-        <img src="/files/icons/social/16/twitter.png" alt="Social Media Icon"
+        <img src="/files/icons/social/16/twitter.png" alt="Share this on Twitter"
              style="border: none;"/>
     </a>
 </div>
@@ -21,7 +21,7 @@
        style="padding: 0; margin: 0; text-decoration: none;" target="_blank"
        title="Share this on Stumble Upon">
         <img src="/files/icons/social/16/stumbleupon.png"
-             alt="Social Media Icon" style="border: none;"/>
+             alt="Share this on Stumble Upon" style="border: none;"/>
 
     </a>
 </div>
@@ -29,7 +29,7 @@
     <a href="http://digg.com/submit?phase=2&amp;url=<?php echo urlencode( get_permalink( ) ); ?>&amp;title=<?php echo urlencode( get_the_title( ) ); ?>"
        style="padding: 0; margin: 0; text-decoration: none;" target="_blank"
        title="Share this on Digg">
-        <img src="/files/icons/social/16/digg.png" alt="Social Media Icon"
+        <img src="/files/icons/social/16/digg.png" alt="Share this on Digg"
              style="border: none;"/>
     </a>
 </div>
@@ -37,7 +37,7 @@
     <a href="http://reddit.com/submit?url=<?php echo urlencode( get_permalink( ) ); ?>&amp;title=<?php echo urlencode( get_the_title( ) ); ?>"
        style="padding: 0; margin: 0; text-decoration: none;" target="_blank"
        title="Share this on Reddit">
-        <img src="/files/icons/social/16/reddit.png" alt="Social Media Icon"
+        <img src="/files/icons/social/16/reddit.png" alt="Share this on Reddit"
              style="border: none;"/>
     </a>
 
@@ -46,7 +46,7 @@
     <a href="http://del.icio.us/post?url=<?php echo urlencode( get_permalink( ) ); ?>&amp;title=<?php echo urlencode( get_the_title( ) ); ?>"
        style="padding: 0; margin: 0; text-decoration: none;" target="_blank"
        title="Share this on Delicious">
-        <img src="/files/icons/social/16/delicious.png" alt="Social Media Icon"
+        <img src="/files/icons/social/16/delicious.png" alt="Share this on Delicious"
              style="border: none;"/>
     </a>
 </div>
