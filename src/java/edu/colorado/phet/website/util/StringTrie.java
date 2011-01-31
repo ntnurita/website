16ea8ce2ef7bed8618c2a42d0dfdfd95935616a0
Jonathan Olson <olsonsjc@gmail.com>
2011-01-30 16:45:10 -0800

    website: added string trie for autocompletion

---
+++ src/java/edu/colorado/phet/website/util/StringTrie.java
@@ -0,0 +1,159 @@
+/*
+ * Copyright 2011, University of Colorado
+ */
+
+package edu.colorado.phet.website.util;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * Unsorted String Trie. This means that given a string 'str', it should be able to efficiently add it to the trie, or
+ * find all contained strings that start with 'str'
+ */
+public class StringTrie {
+    private StringTrieNode root = new StringTrieNode( 0 );
+
+    /**
+     * Returns all strings that start with the substring start
+     *
+     * @param start Starting substring
+     * @return All strings that start with the substring start
+     */
+    public List<String> getStartingWith( String start ) {
+        List<String> ret = new LinkedList<String>();
+        StringTrieNode node = root;
+        for ( char character : start.toCharArray() ) {
+            StringTrieNode next = node.getChild( character );
+            if ( next == null ) {
+                if ( node.isLeaf() && node.string.startsWith( start ) ) {
+                    ret.add( node.string );
+                }
+                node = next;
+                break;
+            }
+            node = next;
+        }
+        if ( node == null ) {
+            return ret;
+        }
+        node.addChildStringsToList( ret );
+        return ret;
+    }
+
+    /**
+     * Adds a string to the trie
+     *
+     * @param str String to add
+     */
+    public void add( String str ) {
+        root.addString( str );
+    }
+
+    @Override
+    public String toString() {
+        return root.toString();
+    }
+}
+
+class StringTrieNode {
+    public String string;
+    public HashMap<Character, StringTrieNode> children;
+    public int depth;
+
+    StringTrieNode( int depth ) {
+        this.depth = depth;
+    }
+
+    public boolean isEmpty() {
+        return string == null && ( children == null );
+    }
+
+    public boolean isLeaf() {
+        return string != null && children == null;
+    }
+
+    public StringTrieNode getChild( char character ) {
+        if ( children == null ) {
+            return null;
+        }
+        return children.get( character );
+    }
+
+    public void addChildStringsToList( List<String> strings ) {
+        if ( string != null ) {
+            strings.add( string );
+        }
+        else {
+            for ( StringTrieNode node : children.values() ) {
+                node.addChildStringsToList( strings );
+            }
+        }
+    }
+
+    private void convertToBranch() {
+        string = null;
+        children = new HashMap<Character, StringTrieNode>();
+    }
+
+    public void addString( String str ) {
+        if ( isEmpty() ) {
+            // we must be the root
+            string = str;
+            return;
+        }
+        if ( isLeaf() ) {
+            // if the string to add is a duplicate of our existing string, bail
+            if ( str.equals( string ) ) {
+                return;
+            }
+
+            // we are a leaf. convert to a branch
+            String other = string;
+            convertToBranch();
+
+            // add the two strings in
+            addString( other );
+            addString( str );
+        }
+        else {
+            // not a leaf node
+            StringTrieNode possibleChild = getChild( str.charAt( depth ) );
+            if ( possibleChild != null ) {
+                // follow the path
+                possibleChild.addString( str );
+            }
+            else {
+                // create a new node
+                StringTrieNode child = new StringTrieNode( depth + 1 );
+                child.string = str;
+                children.put( str.charAt( depth ), child );
+            }
+        }
+    }
+
+    @Override
+    public String toString() {
+        // for verification purposes
+
+        if ( isEmpty() ) {
+            return "empty";
+        }
+        String padding = "";
+        for ( int i = 0; i < depth; i++ ) {
+            padding += " ";
+        }
+        if ( isLeaf() ) {
+            return padding + string + "\n";
+        }
+        else {
+            String ret = "";
+            for ( Character character : children.keySet() ) {
+                ret += padding + character + "\n";
+                ret += children.get( character ).toString();
+            }
+            return ret;
+        }
+    }
+}
---
+++ src/test/edu/colorado/phet/website/tests/StringTrieTest.java
@@ -0,0 +1,39 @@
+/*
+ * Copyright 2011, University of Colorado
+ */
+
+package edu.colorado.phet.website.tests;
+
+import org.junit.Test;
+
+import edu.colorado.phet.website.util.StringTrie;
+
+import static org.junit.Assert.assertEquals;
+
+public class StringTrieTest {
+    @Test
+    public void testStringTrie1() {
+        StringTrie trie = new StringTrie();
+        System.out.println( "---\n" + trie );
+        trie.add( "this is a test" );
+        System.out.println( "---\n" + trie );
+        trie.add( "focus" );
+        System.out.println( "---\n" + trie );
+        trie.add( "this is not a test" );
+        System.out.println( "---\n" + trie );
+        trie.add( "this is a test" );
+        System.out.println( "---\n" + trie );
+        trie.add( "this was a test" );
+        System.out.println( "---\n" + trie );
+
+        assertEquals( trie.getStartingWith( "this" ).size(), 3 );
+        assertEquals( trie.getStartingWith( "this is" ).size(), 2 );
+        assertEquals( trie.getStartingWith( "bark" ).size(), 0 );
+        assertEquals( trie.getStartingWith( "this is a" ).size(), 1 );
+        assertEquals( trie.getStartingWith( "fo" ).size(), 1 );
+        assertEquals( trie.getStartingWith( "focus" ).size(), 1 );
+        assertEquals( trie.getStartingWith( "focused" ).size(), 0 );
+        assertEquals( trie.getStartingWith( "folly" ).size(), 0 );
+        assertEquals( trie.getStartingWith( "" ).size(), 4 );
+    }
+}
