fe1b009bab088725eb1e485e0246a886c0f39211
Jonathan Olson <olsonsjc@gmail.com>
2011-02-01 07:54:34 -0800

    website: added HTTP caching headers for mostly static Tomcat content

--- src/java/edu/colorado/phet/website/services/PostFilter.java
+++ src/java/edu/colorado/phet/website/services/PostFilter.java
@@ -47,8 +47,41 @@ public class PostFilter implements Filter {
 
             logger.debug( requestPath );
 
+            /*---------------------------------------------------------------------------*
+            * caching
+            *----------------------------------------------------------------------------*/
+
+            boolean cachable = requestPath.startsWith( "/images/" )
+                               || requestPath.startsWith( "/js/" )
+                               || requestPath.startsWith( "/css/" )
+                               || requestPath.equals( "/favicon.ico" )
+                               || requestPath.equals( "/crossdomain.xml" );
+
+            if ( cachable ) {
+                // optimizations for caching. see http://code.google.com/speed/page-speed/docs/caching.html
+                long numberOfDaysToCache = 30L;
+
+                long expireTimeIfCachable = System.currentTimeMillis() + ( 3600L * 24L * numberOfDaysToCache * 1000L );
+
+                // make this cachable by proxies between the user and the website
+                responseWrapper.setHeader( "Cache-control", "public" );
+
+                // hard-cache the content
+                responseWrapper.setDateHeader( "Expires", expireTimeIfCachable );
+                // proxies should remember compressed and uncompressed versions of this file, and can respond with either
+                //responseWrapper.addHeader( "Vary", "Accept-Encoding" ); (already added by tomcat or apache)
+            }
+
+            /*---------------------------------------------------------------------------*
+            * filter
+            *----------------------------------------------------------------------------*/
+
             // process the request at lower levels. If lower levels show a 404, it will be recorded in the wrapper
             chain.doFilter( servletRequest, responseWrapper );
+
+            /*---------------------------------------------------------------------------*
+            * handle 404 or other errors
+            *----------------------------------------------------------------------------*/
 
             if ( responseWrapper.isNotFound() ) {
                 // see if we want to redirect this miss. If null, we won't redirect
