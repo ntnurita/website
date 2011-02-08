af43739a18c93ef5e701c643378113f5de992727
Jonathan Olson <olsonsjc@gmail.com>
2011-02-08 11:27:09 -0800

    website: changed testing to check on localhost

--- src/test/edu/colorado/phet/website/tests/SeleniumUtils.java
+++ src/test/edu/colorado/phet/website/tests/SeleniumUtils.java
@@ -20,7 +20,7 @@ import com.thoughtworks.selenium.DefaultSelenium;
 import com.thoughtworks.selenium.Selenium;
 
 public class SeleniumUtils {
-    public static final String WEB_SERVER = "192.168.1.64";
+    public static final String WEB_SERVER = "localhost";
     public static final int PORT = 4444;
 
     public static Selenium createDefaultSelenium() {
