78c920a986c27c2f561e6a9f5cac65d015eb6a5d
Jonathan Olson <olsonsjc@gmail.com>
2011-03-02 14:45:21 -0800

    website: newsletter fixes for handling that many users without massive failures

--- src/java/edu/colorado/phet/website/admin/AdminMainPage.java
+++ src/java/edu/colorado/phet/website/admin/AdminMainPage.java
@@ -142,12 +142,7 @@ public class AdminMainPage extends AdminPage {
                 HibernateUtils.wrapCatchTransaction( getHibernateSession(), new VoidTask() {
                     public Void run( Session session ) {
 //                        String[] emails = new String[]{
-//                                "olsonsjc@gmail.com",
-//                                "jonkaler@live.com",
-//                                "kalequack@yahoo.com",
-//                                "katherine.perkins@colorado.edu",
-//                                "Kathryn.Dessau@colorado.edu"
-//                                "reids@colorado.edu"
+//                                "olsonsjc@gmail.com"
 //                        };
 //                        for ( String email : emails ) {
 //                            PhetUser user = (PhetUser) session.createQuery( "select u from PhetUser as u where u.email = :email" ).setString( "email", email ).uniqueResult();
