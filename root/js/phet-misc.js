88a19350e81b96450fbcfe16b5f7c798afdeeff9
Jonathan Olson <olsonsjc@gmail.com>
2011-03-06 22:45:29 -0800

    website: JS patch for class = external for opening in new window regression

--- root/js/phet-misc.js
+++ root/js/phet-misc.js
@@ -3,6 +3,9 @@ $( document ).ready( function() {
     $( 'a[rel*=external]' ).each( function() {
         $( this ).attr( 'target', '_blank' );
     } );
+    $( 'a[class*=external]' ).each( function() {
+        $( this ).attr( 'target', '_blank' );
+    } );
     $( '.autocompleteOff' ).each( function() {
         $( this ).attr( 'autocomplete', 'off' );
     } );
