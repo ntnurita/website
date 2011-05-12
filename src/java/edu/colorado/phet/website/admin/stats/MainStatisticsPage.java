ab0a8018d6104dbe2d45829f41845c2961d3924c
Jonathan Olson <olsonsjc@gmail.com>
2011-05-11 19:45:25 -0700

    website: added an admin statistics page

---
+++ src/java/edu/colorado/phet/website/admin/stats/MainStatisticsPage.java
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2011, University of Colorado
+ */
+
+package edu.colorado.phet.website.admin.stats;
+
+import java.util.*;
+
+import org.apache.log4j.Logger;
+import org.apache.wicket.PageParameters;
+import org.apache.wicket.markup.html.basic.Label;
+import org.hibernate.Session;
+
+import edu.colorado.phet.website.admin.AdminPage;
+import edu.colorado.phet.website.data.LocalizedSimulation;
+import edu.colorado.phet.website.data.Simulation;
+import edu.colorado.phet.website.data.contribution.Contribution;
+import edu.colorado.phet.website.util.hibernate.HibernateUtils;
+import edu.colorado.phet.website.util.hibernate.VoidTask;
+
+/**
+ * A page that shows some elementary statistics about the website, simulations, activities and translations.
+ *
+ * In general, if I'm asked about a statistic and I can compute it on this page, I'll probably add it here so I won't
+ * need to compute it more.
+ */
+public class MainStatisticsPage extends AdminPage {
+
+    private static final Logger logger = Logger.getLogger( MainStatisticsPage.class.getName() );
+
+    public MainStatisticsPage( PageParameters parameters ) {
+        super( parameters );
+
+        HibernateUtils.wrapTransaction( getHibernateSession(), new VoidTask() {
+            public Void run( Session session ) {
+                final List simList = session.createQuery( "select s from Simulation as s" ).list();
+                final List activityList = session.createQuery( "select c from Contribution as c" ).list();
+                final Set<Locale> usedSimulationLocales = new HashSet<Locale>();
+                final List<LocalizedSimulation> localizedSimulations = new LinkedList<LocalizedSimulation>();
+                List<Simulation> simulations = new LinkedList<Simulation>() {{
+                    for ( Object o : simList ) {
+                        Simulation simulation = (Simulation) o;
+                        if ( simulation.isVisible() ) { // note: checks project visibility too, don't reduce
+                            add( simulation );
+                            for ( Object o1 : simulation.getLocalizedSimulations() ) {
+                                LocalizedSimulation lsim = (LocalizedSimulation) o1;
+                                localizedSimulations.add( lsim );
+                                usedSimulationLocales.add( lsim.getLocale() );
+                            }
+                        }
+                    }
+                }};
+                List<Contribution> contributions = new LinkedList<Contribution>( activityList );
+                int phetContributionsCount = 0;
+                for ( Contribution contribution : contributions ) {
+                    if ( contribution.isFromPhet() ) {
+                        phetContributionsCount++;
+                    }
+                }
+
+                add( new Label( "sim-count", "Total visible simulations: " + simulations.size() ) );
+                add( new Label( "activity-count", "Total activities: " + contributions.size() ) );
+                add( new Label( "phet-activity-count", "PhET activities: " + phetContributionsCount ) );
+                add( new Label( "total-sim-languages", "Number of languages (including English) that sims are translated into: " + usedSimulationLocales.size() ) );
+                add( new Label( "total-sim-translations", "Number of sim translations (including English): " + localizedSimulations.size() ) );
+
+                return null;
+            }
+        } );
+    }
+
+}
