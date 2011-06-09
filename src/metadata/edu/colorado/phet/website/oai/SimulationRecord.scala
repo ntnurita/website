0e67e514a46b40b929af9f9141cd85561a5dff3a
Jonathan Olson <olsonsjc@gmail.com>
2011-06-08 21:15:59 -0700

    website: xml filtering work with simulation record (metadata)

--- src/metadata/edu/colorado/phet/website/oai/SimulationRecord.scala
+++ src/metadata/edu/colorado/phet/website/oai/SimulationRecord.scala
@@ -1,6 +1,6 @@
 package edu.colorado.phet.website.oai
 
-import xml.XML
+import xml.{NodeSeq, XML}
 
 /**
  * Represents a simulation record from the master data
@@ -8,11 +8,12 @@ import xml.XML
 class SimulationRecord(str: String) {
   val xml = XML.loadString(str)
 
-  println("woohoo")
-  println("xml: " + xml)
-  println("xml \\ title: " + ( xml \ "title" ))
+  println("filtered: " + ( ( xml \ "title" ) \ "string" ).filter(node => ( node \ "@locale" ).text == "en"))
+  println("english title: " + englishString(xml \ "title"))
 
   // TODO: implement
 
   def englishTitle = "Fake Title"
+
+  def englishString(node: NodeSeq): String = ( node \ "string" ).filter(node => ( node \ "@locale" ).text == "en").text
 }
