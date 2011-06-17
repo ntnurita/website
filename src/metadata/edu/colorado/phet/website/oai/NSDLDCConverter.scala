d4d7aba74abdc47dcbea71dc3211df93e94428a1
Jonathan Olson <olsonsjc@gmail.com>
2011-06-17 10:05:53 -0700

    website: NSDL added audience, added comments inline

--- src/metadata/edu/colorado/phet/website/oai/NSDLDCConverter.scala
+++ src/metadata/edu/colorado/phet/website/oai/NSDLDCConverter.scala
@@ -14,7 +14,6 @@ class NSDLDCConverter extends PhetFormatConverter {
   def convertRecord(record: SimulationRecord, servletContext: ServletContext): String = {
     // TODO: subject (for categories)
     // TODO: education level
-    // TODO: audience
     // TODO: type. DCMI maybe has <dc:type>InteractiveResource</dc:type>
     // TODO: verify that rights section is ok for now
     // TODO: access rights (NSDL specific)
@@ -30,27 +29,57 @@ class NSDLDCConverter extends PhetFormatConverter {
                      xmlns:dct="http://purl.org/dc/terms/"
                      xmlns:ieee="http://ltsc.ieee.org/xsd/LOM"
                      xsi:schemaLocation="http://ns.nsdl.org/nsdl_dc_v1.02/ http://ns.nsdl.org/schemas/nsdl_dc/nsdl_dc_v1.02.xsd">
+      <!-- -->
+      <!-- Unique identifier for the sim (the sim URI) -->
       <dc:identifier xsi:type="dct:URI">{record.simPageLink}</dc:identifier>
+
+      <!-- English title -->
       <dc:title xml:lang="en">{record.englishTitle}</dc:title>
+      <!-- TODO: change the order of translated strings so English is first and we can get rid of this exception-->
       {record.translatedTitles.filter(str => str.language != "en").map(str => <dc:title xml:lang={str.language}>{str.string}</dc:title>)}
+
+      <!-- Descriptions -->
       <dc:description xml:lang="en">{record.englishDescrption}</dc:description>
       {record.translatedDescriptions.filter(str => str.language != "en").map(str => <dc:description xml:lang={str.language}>{str.string}</dc:description>)}
+
+      <!-- Rights -->
       <dc:rights>Free access / usage by everyone</dc:rights>
       <dc:rights>More licensing information available at http://phet.colorado.edu/en/about/licensing</dc:rights>
       <dc:rights>© 2011 University of Colorado</dc:rights>
+
+      <!-- Credits -->
       <dc:publisher>PhET Interactive Simulations</dc:publisher>
       <dc:creator>PhET Interactive Simulations</dc:creator>
       {record.authors.map(author => <dc:creator>{author}</dc:creator>)}
+
+      <!-- Available languages-->
       {record.languages.map(language => <dc:language>{language}</dc:language>)}
+
+      <!-- Creation date (when the sim was first deployed to our production site) -->
       <dc:date xsi:type="dct:W3CDTF">{record.timeCreated}</dc:date>
       <dct:created xsi:type="dct:W3CDTF">{record.timeCreated}</dct:created>
+
+      <!-- Modification date (includes changes of translations, actual simulation modifications, etc.) -->
       <dct:modified xsi:type="dct:W3CDTF">{record.timeUpdated}</dct:modified>
+
+      <!-- Mime types used -->
       {record.mimeTypes.map(mimeType => <dc:format>{mimeType}</dc:format>)}
+
+      <!-- Simulation version -->
       <dct:hasVersion>{record.versionString}</dct:hasVersion>
+
+      <!-- Interaction information-->
       <ieee:interactivityType><ieee:source>LOMv1.0</ieee:source><ieee:source>active</ieee:source></ieee:interactivityType>
       <ieee:interactivityLevel><ieee:source>LOMv1.0</ieee:source><ieee:source>very high</ieee:source></ieee:interactivityLevel>
       <ieee:typicalLearningTime><!-- TODO Duration type, like PT1H30M or PT1M45S--></ieee:typicalLearningTime>
+
+      <!-- Keywords -->
       {record.translatedTerms.map(term => term.map(str => <dc:subject xml:lang={str.language}>{str.string}</dc:subject>))}
+
+      <!-- Recommended Audiences -->
+      <dct:audience xsi:type="nsdl_dc:NSDLAudience">Learner</dct:audience>
+      <dct:audience xsi:type="nsdl_dc:NSDLAudience">Educator</dct:audience>
+      <dct:audience xsi:type="nsdl_dc:NSDLAudience">General Public</dct:audience>
     </nsdl_dc:nsdl_dc>.toString
   }
 }
