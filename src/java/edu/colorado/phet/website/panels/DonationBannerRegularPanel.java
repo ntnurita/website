77c94d98120f582ceb7975d59e4cccbf7a787fec
Jonathan Olson <olsonsjc@gmail.com>
2011-11-15 22:34:40 -0800

    website: improvements for the donation banner. switching of banner based on page

---
+++ src/java/edu/colorado/phet/website/panels/DonationBannerRegularPanel.java
@@ -0,0 +1,16 @@
+// Copyright 2002-2011, University of Colorado
+
+package edu.colorado.phet.website.panels;
+
+import org.apache.log4j.Logger;
+
+import edu.colorado.phet.website.util.PageContext;
+
+public class DonationBannerRegularPanel extends PhetPanel {
+
+    private static final Logger logger = Logger.getLogger( DonationBannerRegularPanel.class.getName() );
+
+    public DonationBannerRegularPanel( String id, final PageContext context ) {
+        super( id, context );
+    }
+}
--- src/java/edu/colorado/phet/website/templates/PhetPage.java
+++ src/java/edu/colorado/phet/website/templates/PhetPage.java
@@ -38,6 +38,8 @@ import edu.colorado.phet.website.content.IndexPage;
 import edu.colorado.phet.website.content.getphet.FullInstallPanel;
 import edu.colorado.phet.website.data.Translation;
 import edu.colorado.phet.website.menu.NavMenu;
+import edu.colorado.phet.website.panels.DonationBannerHomePanel;
+import edu.colorado.phet.website.panels.DonationBannerRegularPanel;
 import edu.colorado.phet.website.panels.LogInOutPanel;
 import edu.colorado.phet.website.panels.SearchPanel;
 import edu.colorado.phet.website.translation.PhetLocalizer;
@@ -233,6 +235,13 @@ public abstract class PhetPage extends WebPage implements Stylable {
         }
         else {
             add( new WebMarkupContainer( "js" ) );
+        }
+
+        if ( this instanceof IndexPage ) {
+            add( new DonationBannerHomePanel( "donation-banner", getPageContext() ) );
+        }
+        else {
+            add( new DonationBannerRegularPanel( "donation-banner", getPageContext() ) );
         }
     }
 
