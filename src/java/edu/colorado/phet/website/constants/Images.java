982d65b1060a1aeaf9c6b6e868fc3a195171e6f4
Jonathan Olson <olsonsjc@gmail.com>
2011-11-17 22:35:32 -0800

    website: updated about-news page with the new newsletter, thumbnail, etc.

--- src/java/edu/colorado/phet/website/constants/Images.java
+++ src/java/edu/colorado/phet/website/constants/Images.java
@@ -27,6 +27,7 @@ public class Images {
     public static final ImageHandle NEWSLETTER_SUMMER_10 = new ImageHandle( "/images/phet_newsletter_summer_10_thumbnail.png", true );
     public static final ImageHandle NEWSLETTER_MARCH_11 = new ImageHandle( "/images/phet_newsletter_march_11_thumbnail.png", true );
     public static final ImageHandle NEWSLETTER_MAY_11 = new ImageHandle( "/images/phet_newsletter_may_11_thumbnail.png", true );
+    public static final ImageHandle NEWSLETTER_NOVEMBER_11 = new ImageHandle( "/images/phet_newsletter_november_11_thumbnail.png", true );
 
     public static final ImageHandle PHET_LOGO_ICON_SMALL = new ImageHandle( "/images/contributions/phet-logo-icon-small.jpg", true );
     public static final ImageHandle GOLD_STAR_SMALL = new ImageHandle( "/images/contributions/gold-star-small.jpg", true );
