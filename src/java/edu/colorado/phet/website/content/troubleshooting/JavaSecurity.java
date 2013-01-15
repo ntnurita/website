bd3d33eb1a67735f386a29943e7c17645322e9e5
Jonathan Olson <olsonsjc@gmail.com>
2013-01-14 21:52:51 -0800

    Opening links in new tabs

--- src/java/edu/colorado/phet/website/content/troubleshooting/JavaSecurity.java
+++ src/java/edu/colorado/phet/website/content/troubleshooting/JavaSecurity.java
@@ -16,15 +16,15 @@ public class JavaSecurity extends PhetPanel {
         super( id, context );
 
         add( new LocalizedText( "troubleshooting-javaSecurity-intro", "troubleshooting.javaSecurity.intro", new Object[] {
-            "href=\"http://www.us-cert.gov/cas/techalerts/TA13-010A.html\""
+            "href=\"http://www.us-cert.gov/cas/techalerts/TA13-010A.html\" target=\"blank\""
         } ) );
 
         add( new LocalizedText( "troubleshooting-javaSecurity-upgradeStep", "troubleshooting.javaSecurity.upgradeStep", new Object[] {
-            "href=\"http://www.java.com/en/download/index.jsp\""
+            "href=\"http://www.java.com/en/download/index.jsp\" target=\"blank\""
         } ) );
 
         add( new LocalizedText( "troubleshooting-javaSecurity-disableStep", "troubleshooting.javaSecurity.disableStep", new Object[] {
-            "href=\"http://www.zdnet.com/how-to-disable-java-in-your-browser-on-windows-mac_p5-7000009732/\""
+            "href=\"http://www.zdnet.com/how-to-disable-java-in-your-browser-on-windows-mac_p5-7000009732/\" target=\"blank\""
         } ) );
         
         add( new LocalizedText( "troubleshooting-javaSecurity-questions", "troubleshooting.javaSecurity.questions", new Object[] {
