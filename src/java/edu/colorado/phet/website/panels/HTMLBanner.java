87ed2c769a28ba6ecc1f122a4ae9b812a315832b
Jonathan Olson <olsonsjc@gmail.com>
2013-08-14 10:53:06 -0600

    website: proper linking of HTML5 banner

--- src/java/edu/colorado/phet/website/panels/HTMLBanner.java
+++ src/java/edu/colorado/phet/website/panels/HTMLBanner.java
@@ -3,19 +3,23 @@
 package edu.colorado.phet.website.panels;
 
 import org.apache.log4j.Logger;
+import org.apache.wicket.markup.html.link.Link;
 
 import edu.colorado.phet.website.components.StaticImage;
 import edu.colorado.phet.website.constants.Images;
+import edu.colorado.phet.website.content.simulations.HTML5Page;
 import edu.colorado.phet.website.util.PageContext;
 import edu.colorado.phet.website.util.WebImage;
 
 public class HTMLBanner extends PhetPanel {
 
-    private static final Logger logger = Logger.getLogger( DonationBannerRegularPanel.class.getName() );
+    private static final Logger logger = Logger.getLogger( HTMLBanner.class.getName() );
 
     public HTMLBanner( String id, final PageContext context ) {
         super( id, context );
 
-        add( new StaticImage( "html-banner-image", WebImage.get( Images.HTML5_LOGO_80 ), null ) );
+        Link link = HTML5Page.getLinker().getLink( "html-banner-link", context, getPhetCycle() );
+        add( link );
+        link.add( new StaticImage( "html-banner-image", WebImage.get( Images.HTML5_LOGO_80 ), null ) );
     }
 }
