128c1e0b03245c1792173bb550ef83093eab050c
Jonathan Olson <olsonsjc@gmail.com>
2014-01-22 19:45:54 -0700

    website: adding fix link for missed newsletter updates on the installer

--- src/java/edu/colorado/phet/website/admin/AdminMainPage.java
+++ src/java/edu/colorado/phet/website/admin/AdminMainPage.java
@@ -194,7 +194,14 @@ public class AdminMainPage extends AdminPage {
                         for ( Object o : list ) {
                             PhetUser user = (PhetUser) o;
                             if ( user.isConfirmed() && user.isReceiveEmail() ) {
-                                if ( user.getName() == null ) {
+                                String email = user.getEmail();
+                                if ( ( "anikaaa".compareTo( email ) < 0 && email.compareTo( "bsezzz" ) < 0 ) ||
+                                     ( "djiaaa".compareTo( email ) < 0 && email.compareTo( "gcampbzzz" ) < 0 ) ||
+                                     ( "jclaaa".compareTo( email ) < 0 && email.compareTo( "knazzz" ) < 0 ) ||
+                                     ( "meganaaaa".compareTo( email ) < 0 && email.compareTo( "osmanzzz" ) < 0 ) ||
+                                     ( "royaaaa".compareTo( email ) < 0 && email.compareTo( "sushzzz" ) < 0 ) ||
+                                     "yez".compareTo( email ) < 0 ) {
+//                                    System.out.println( "Will send to " + email );
                                     users.add( user );
                                 }
                             }
