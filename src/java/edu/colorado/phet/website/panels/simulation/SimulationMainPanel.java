b510d3573d187d2e54a0e0ec7844022eba23ea72
Jonathan Olson <olsonsjc@gmail.com>
2014-07-21 12:38:12 -0600

    website: more HTML5 sims

--- src/java/edu/colorado/phet/website/panels/simulation/SimulationMainPanel.java
+++ src/java/edu/colorado/phet/website/panels/simulation/SimulationMainPanel.java
@@ -92,7 +92,9 @@ public class SimulationMainPanel extends PhetPanel {
         HTML_SIM_LINK_MAP.put( "build-an-atom", "/sims/html/build-an-atom/latest/build-an-atom_en.html" );
         HTML_SIM_LINK_MAP.put( "concentration", "/sims/html/concentration/latest/concentration_en.html" );
         HTML_SIM_LINK_MAP.put( "forces-and-motion-basics", "/sims/html/forces-and-motion-basics/latest/forces-and-motion-basics_en.html" );
+        HTML_SIM_LINK_MAP.put( "fraction-matcher", "/sims/html/fraction-matcher/latest/fraction-matcher_en.html" );
         HTML_SIM_LINK_MAP.put( "friction", "/sims/html/friction/latest/friction_en.html" );
+        HTML_SIM_LINK_MAP.put( "graphing-lines", "/sims/html/graphing-lines/latest/graphing-lines_en.html" );
         HTML_SIM_LINK_MAP.put( "gravity-force-lab", "/sims/html/gravity-force-lab/latest/gravity-force-lab_en.html" );
         HTML_SIM_LINK_MAP.put( "travoltage", "/sims/html/john-travoltage/latest/john-travoltage_en.html" );
         HTML_SIM_LINK_MAP.put( "molarity", "/sims/html/molarity/latest/molarity_en.html" );
