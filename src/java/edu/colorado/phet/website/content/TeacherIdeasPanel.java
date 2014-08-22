795c64f0e11104a23f576831fd499710c2c8e693
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-08-21 19:52:57 -0600

    Add linkers to TeacherIdeasPanel

--- src/java/edu/colorado/phet/website/content/TeacherIdeasPanel.java
+++ src/java/edu/colorado/phet/website/content/TeacherIdeasPanel.java
@@ -4,11 +4,18 @@
 
 package edu.colorado.phet.website.content;
 
+import org.apache.wicket.AttributeModifier;
+import org.apache.wicket.markup.html.WebMarkupContainer;
+import org.apache.wicket.markup.html.link.Link;
+import org.apache.wicket.model.Model;
+
 import edu.colorado.phet.website.components.LocalizedText;
 import edu.colorado.phet.website.constants.Linkers;
 import edu.colorado.phet.website.content.contribution.ContributionBrowsePage;
 import edu.colorado.phet.website.content.contribution.ContributionCreatePage;
 import edu.colorado.phet.website.content.contribution.ContributionGuidelinesPanel;
+import edu.colorado.phet.website.content.forteachers.ActivitiesdesignPanel;
+import edu.colorado.phet.website.content.forteachers.TipsPanel;
 import edu.colorado.phet.website.content.simulations.CategoryPage;
 import edu.colorado.phet.website.panels.PhetPanel;
 import edu.colorado.phet.website.util.PageContext;
@@ -19,27 +26,28 @@ public class TeacherIdeasPanel extends PhetPanel {
     public TeacherIdeasPanel( String id, PageContext context ) {
         super( id, context );
 
-        add( new LocalizedText( "welcome", "teacherIdeas.welcome", new Object[] {
-                CategoryPage.getDefaultLinker().getHref( context, getPhetCycle() )
-        } ) );
-
-        add( new LocalizedText( "start", "teacherIdeas.start", new Object[] {
-                ContributionBrowsePage.getLinker().getHref( context, getPhetCycle() ),
-                CategoryPage.getDefaultLinker().getHref( context, getPhetCycle() )
-        } ) );
-
-        add( new LocalizedText( "contribute", "teacherIdeas.contribute", new Object[] {
-                ContributionCreatePage.getLinker().getHref( context, getPhetCycle() )
-        } ) );
-
-        add( new LocalizedText( "guidelines", "teacherIdeas.guidelines", new Object[] {
-                ContributionGuidelinesPanel.getLinker().getHref( context, getPhetCycle() ),
-                Linkers.CONTRIBUTION_GUIDELINES_PDF.getHref( context, getPhetCycle() )
-        } ) );
-
         // add linkers
         add( CategoryPage.getLinker( "new" ).getLink( "new-sims-link", context, getPhetCycle() ) );
+        add( ActivitiesdesignPanel.getLinker().getLink( "activities-guide-link", context, getPhetCycle() ) );
 
+        // add linkers to table elements using WebMarkupContainer
+        String tipsLink = TipsPanel.getLinker().getRawUrl( context, getPhetCycle() );
+        WebMarkupContainer tipsContainer = new WebMarkupContainer( "tips-button" );
+        tipsContainer.add( TipsPanel.getLinker().getLink( "tipsforusingphet-link", context, getPhetCycle() ) );
+        tipsContainer.add( new AttributeModifier( "onclick", true, new Model<String>( "document.location = '" + tipsLink + "'" ) ) );
+        add( tipsContainer );
+
+        String browseActivitiesLink = ContributionBrowsePage.getLinker().getRawUrl( context, getPhetCycle() );
+        WebMarkupContainer browseActivitiesContainer = new WebMarkupContainer( "browse-activities-button" );
+        browseActivitiesContainer.add( TipsPanel.getLinker().getLink( "browse-activities-link", context, getPhetCycle() ) );
+        browseActivitiesContainer.add( new AttributeModifier( "onclick", true, new Model<String>( "document.location = '" + browseActivitiesLink + "'" ) ) );
+        add( browseActivitiesContainer );
+
+        String submitActivityLink = ContributionCreatePage.getLinker().getRawUrl( context, getPhetCycle() );
+        WebMarkupContainer submitActivityContainer = new WebMarkupContainer( "submit-activity-button" );
+        submitActivityContainer.add( TipsPanel.getLinker().getLink( "submit-activity-link", context, getPhetCycle() ) );
+        submitActivityContainer.add( new AttributeModifier( "onclick", true, new Model<String>( "document.location = '" + submitActivityLink + "'" ) ) );
+        add( submitActivityContainer );
     }
 
     public static String getKey() {
