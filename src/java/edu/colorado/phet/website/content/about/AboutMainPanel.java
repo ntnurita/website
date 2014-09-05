c1c5671b699937e192a24a54ad03f9dbf5fb148c
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-09-05 12:45:33 -0600

    comment out logic in AboutMainPanel.java

--- src/java/edu/colorado/phet/website/content/about/AboutMainPanel.java
+++ src/java/edu/colorado/phet/website/content/about/AboutMainPanel.java
@@ -37,61 +37,61 @@ public class AboutMainPanel extends PhetPanel {
     public AboutMainPanel( String id, PageContext context ) {
         super( id, context );
 
-        add( new LocalizedText( "about-p1", "about.p1", new Object[] {
-                ResearchPanel.getLinker().getHref( context, getPhetCycle() )
-        } ) );
-
-        add( new LocalizedText( "about-p2", "about.p2" ) );
-
-        add( new LocalizedText( "about-p3", "about.p3", new Object[] {
-                AboutLegendPanel.getLinker().getHref( context, getPhetCycle() )
-        } ) );
-
-        add( new LocalizedText( "about-p4", "about.p4", new Object[] {
-                IndexPage.getLinker().getHref( context, getPhetCycle() ),
-                TroubleshootingJavaPanel.getLinker().getHref( context, getPhetCycle() ),
-                TroubleshootingFlashPanel.getLinker().getHref( context, getPhetCycle() )
-        } ) );
-
-        final List<PhetUser> members = new LinkedList<PhetUser>();
-
-        HibernateUtils.wrapTransaction( getHibernateSession(), new HibernateTask() {
-            public boolean run( Session session ) {
-                List list = session.createQuery( "select u from PhetUser as u where u.teamMember = true" ).list();
-                for ( Object o : list ) {
-                    members.add( (PhetUser) o );
-                }
-                return true;
-            }
-        } );
-
-        Collections.sort( members, new Comparator<PhetUser>() {
-            public int compare( PhetUser a, PhetUser b ) {
-                return a.getName().compareTo( b.getName() );
-            }
-        } );
-
-        final boolean isAdmin = PhetSession.get().isSignedIn() && PhetSession.get().getUser().isTeamMember();
-
-        add( new ListView<PhetUser>( "person", members ) {
-            protected void populateItem( ListItem<PhetUser> item ) {
-                PhetUser user = item.getModelObject();
-                item.add( new Label( "name", user.getName() ) );
-                item.add( new Label( "title", user.getJobTitle() ) );
-                if ( isAdmin ) {
-                    Label marker = new Label( "show-admin", "" );
-                    marker.setRenderBodyOnly( true );
-                    item.add( marker );
-                    item.add( new Label( "primary-phone", user.getPhone1() ) );
-                    item.add( new Label( "secondary-phone", user.getPhone2() ) );
-                }
-                else {
-                    item.add( new InvisibleComponent( "show-admin" ) );
-                    item.add( new InvisibleComponent( "primary-phone" ) );
-                    item.add( new InvisibleComponent( "secondary-phone" ) );
-                }
-            }
-        } );
+//        add( new LocalizedText( "about-p1", "about.p1", new Object[] {
+//                ResearchPanel.getLinker().getHref( context, getPhetCycle() )
+//        } ) );
+//
+//        add( new LocalizedText( "about-p2", "about.p2" ) );
+//
+//        add( new LocalizedText( "about-p3", "about.p3", new Object[] {
+//                AboutLegendPanel.getLinker().getHref( context, getPhetCycle() )
+//        } ) );
+//
+//        add( new LocalizedText( "about-p4", "about.p4", new Object[] {
+//                IndexPage.getLinker().getHref( context, getPhetCycle() ),
+//                TroubleshootingJavaPanel.getLinker().getHref( context, getPhetCycle() ),
+//                TroubleshootingFlashPanel.getLinker().getHref( context, getPhetCycle() )
+//        } ) );
+//
+//        final List<PhetUser> members = new LinkedList<PhetUser>();
+//
+//        HibernateUtils.wrapTransaction( getHibernateSession(), new HibernateTask() {
+//            public boolean run( Session session ) {
+//                List list = session.createQuery( "select u from PhetUser as u where u.teamMember = true" ).list();
+//                for ( Object o : list ) {
+//                    members.add( (PhetUser) o );
+//                }
+//                return true;
+//            }
+//        } );
+//
+//        Collections.sort( members, new Comparator<PhetUser>() {
+//            public int compare( PhetUser a, PhetUser b ) {
+//                return a.getName().compareTo( b.getName() );
+//            }
+//        } );
+//
+//        final boolean isAdmin = PhetSession.get().isSignedIn() && PhetSession.get().getUser().isTeamMember();
+//
+//        add( new ListView<PhetUser>( "person", members ) {
+//            protected void populateItem( ListItem<PhetUser> item ) {
+//                PhetUser user = item.getModelObject();
+//                item.add( new Label( "name", user.getName() ) );
+//                item.add( new Label( "title", user.getJobTitle() ) );
+//                if ( isAdmin ) {
+//                    Label marker = new Label( "show-admin", "" );
+//                    marker.setRenderBodyOnly( true );
+//                    item.add( marker );
+//                    item.add( new Label( "primary-phone", user.getPhone1() ) );
+//                    item.add( new Label( "secondary-phone", user.getPhone2() ) );
+//                }
+//                else {
+//                    item.add( new InvisibleComponent( "show-admin" ) );
+//                    item.add( new InvisibleComponent( "primary-phone" ) );
+//                    item.add( new InvisibleComponent( "secondary-phone" ) );
+//                }
+//            }
+//        } );
 
         // add linkers
         add( TechAwardPage.getLinker().getLink( "tech-award-link", context, getPhetCycle() ) );
