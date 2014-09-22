3807ee0ec17462eb750c7cc53fc8c81abdb1f3e2
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-09-22 10:10:12 -0600

    AboutMainPanel extends ForTeachersPanel to hide social icons and set correct width

--- src/java/edu/colorado/phet/website/content/about/AboutMainPanel.java
+++ src/java/edu/colorado/phet/website/content/about/AboutMainPanel.java
@@ -5,43 +5,21 @@
 package edu.colorado.phet.website.content.about;
 
 import edu.colorado.phet.website.DistributionHandler;
-import edu.colorado.phet.website.components.LocalizedText;
-import edu.colorado.phet.website.content.IndexPage;
-import edu.colorado.phet.website.content.ResearchPanel;
+import edu.colorado.phet.website.content.forteachers.ForTeachersPanel;
 import edu.colorado.phet.website.content.media.TechAwardPage;
-import edu.colorado.phet.website.panels.PhetPanel;
-import edu.colorado.phet.website.templates.PhetMenuPage;
 import edu.colorado.phet.website.util.PageContext;
 import edu.colorado.phet.website.util.PhetRequestCycle;
 import edu.colorado.phet.website.util.links.AbstractLinker;
 import edu.colorado.phet.website.util.links.RawLinkable;
 
-public class AboutMainPanel extends PhetPanel {
+// Note that this class extends ForTeachersPanel because it sets the correct width and hides the social media
+// icons by default, even though it is not a for-teachers page. ForTeachersPanel should probably be renamed at some point.
+public class AboutMainPanel extends ForTeachersPanel {
     public AboutMainPanel( String id, PageContext context ) {
         super( id, context );
 
         // add linkers
         add( TechAwardPage.getLinker().getLink( "tech-award-link", context, getPhetCycle() ) );
-
-        add( new LocalizedText( "about-p1", "about.p1", new Object[] {
-                ResearchPanel.getLinker().getHref( context, getPhetCycle() )
-        } ) );
-
-        add( new LocalizedText( "about-p2", "about.p2" ) );
-
-        add( new LocalizedText( "about-p3", "about.p3", new Object[] {
-                AboutLegendPanel.getLinker().getHref( context, getPhetCycle() )
-        } ) );
-
-        add( new LocalizedText( "about-p4", "about.p4", new Object[] {
-                IndexPage.getLinker().getHref( context, getPhetCycle() )
-        } ) );
-    }
-    @Override
-    protected void onBeforeRender() {
-        super.onBeforeRender();
-        ((PhetMenuPage) this.getPage()).hideSocialBookmarkButtons();
-        ((PhetMenuPage) this.getPage()).setContentWidth(1120);
     }
 
     public static String getKey() {
