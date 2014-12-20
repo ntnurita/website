e2440d0b28b476085f1bc2636905aafb20d21e9a
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2014-12-19 17:01:45 -0700

    remove new front page strings from string changes so they don't slow down deploying

--- src/java/edu/colorado/phet/website/StringChanges.java
+++ src/java/edu/colorado/phet/website/StringChanges.java
@@ -23,59 +23,6 @@ public class StringChanges {
     public static void checkNewStrings() {
         Session session = HibernateUtils.getInstance().openSession();
 
-        StringUtils.addString( session, "social.blog.tooltip", "Visit our blog" );
-        StringUtils.addString( session, "social.newsletter.tooltip", "Subscribe to our newsletter" );
-        StringUtils.addString( session, "social.pinterest.tooltip", "Pinterest" );
-
-        StringUtils.deleteString( session, "home.playWithSims" );
-        StringUtils.addString( session, "home.playWithSims", "Play with Simulations" );
-
-        StringUtils.deleteString( session, "home.facebookText" );
-        StringUtils.deleteString( session, "home.twitterText" );
-        StringUtils.deleteString( session, "home.blogText" );
-        StringUtils.deleteString( session, "home.subheader" );
-
-        StringUtils.addString( session, "home.blogText", "our blog" );
-        StringUtils.addString( session, "home.newsletterText", "newsletter" );
-        StringUtils.addString( session, "home.facebookText", "find us" );
-        StringUtils.addString( session, "home.youtubeText", "watch us" );
-        StringUtils.addString( session, "home.twitterText", "follow us" );
-        StringUtils.addString( session, "home.pinterestText", "pin us" );
-
-        StringUtils.deleteString( session, "html5.tryOurNewHTML5Sims" );
-        StringUtils.addString( session, "html5.tryOurNewHTML5Sims", "HTML5 Sims" );
-
-        StringUtils.deleteString( session, "html5.crowdfundedProjects" );
-        StringUtils.addString( session, "html5.crowdfundedProjects", "Support PhET's Annual Campaign:" );
-
-        StringUtils.addString( session, "html5.donateToday", "Donate Today" );
-
-        StringUtils.deleteString( session, "html5.bringCCK" );
-        StringUtils.deleteString( session, "html5.buildTeachWithPhet" );
-
-        StringUtils.addString( session, "social.homepage.blog.tooltip", "Visit our blog" );
-        StringUtils.addString( session, "social.homepage.newsletter.tooltip", "Subscribe to our newsletter" );
-        StringUtils.addString( session, "social.homepage.facebook.tooltip", "Find us on Facebook" );
-        StringUtils.addString( session, "social.homepage.youtube.tooltip", "Watch us on Youtube" );
-        StringUtils.addString( session, "social.homepage.twitter.tooltip", "Follow us on Twitter" );
-        StringUtils.addString( session, "social.homepage.pinterest.tooltip", "Pin us on Pinterest" );
-
-        StringUtils.addString( session, "donation-banner.html5.tooltip", "Go to HTML5 Sims" );
-
-        StringUtils.overwriteString( session, "home.simulationsDelivered", "Over 110 million simulations delivered", "Over {0} million simulations delivered" );
-
-        StringUtils.deleteString( session, "installer.mostUpToDate" );
-        StringUtils.addString( session, "installer.mostUpToDate", "Created {0,date,short}. Updates available <a {1}>online</a>." );
-
-        StringUtils.addString( session, "home.about.research", "Research on PhET" );
-
-        StringUtils.deleteString( session, "social.digg.tooltip" );
-        StringUtils.deleteString( session, "social.delicious.tooltip" );
-
-        StringUtils.deleteString( session, "search.search" );
-        StringUtils.addString( session, "search.search", "Search" );
-        StringUtils.addString( session, "search.label", "Search the PhET Website" );
-
         session.close();
     }
 
