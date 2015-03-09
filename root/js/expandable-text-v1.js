b558bd8effd753be2248dc0047287b2a8d3dc9c8
Brian Newsom <brian.newsom@colorado.edu>
2015-03-08 18:00:52 -0600

    Update FAQ page w/ dropdowns, see #74

--- root/js/expandable-text-v1.js
+++ root/js/expandable-text-v1.js
@@ -6,39 +6,46 @@
  * @returns {boolean}
  */
 function toggleMe( id ) {
-  var e = document.getElementById( id );
-  if ( !e || window['answer_' + id] ) {
-    return true;
-  }
-  if ( !e.style.height || e.style.height === "0px" ) {
-    window['answer_' + id] = true;
-    var expandInterval = setInterval( function() {
-      var h = e.offsetHeight;
-      var sh = e.scrollHeight;
-      if ( h < sh ) {
-        h += 20;
-      }
-      else {
-        clearInterval( expandInterval );
-        window['answer_' + id] = false;
-      }
-      e.style.height = h + "px";
-    }, 8 );
-  }
-  else {
-    window['answer_' + id] = true;
-    var retractInterval = setInterval( function() {
-      var h = e.offsetHeight;
-      if ( h > 0 ) {
-        h -= 20;
-      }
-      else {
-        e.style.height = "0px";
-        clearInterval( retractInterval );
-        window['answer_' + id] = false;
-      }
-      e.style.height = h + "px";
-    }, 8 );
-  }
-  return false;
+    var e = document.getElementById( id );
+    var thisElement = document.getElementById( id + '-header' );
+    var newClass;
+    if ( !e || window['answer_' + id] ) {
+        return true;
+    }
+    if ( !e.style.height || e.style.height === "0px" ) {
+        window.location.hash = '#' + id + '-header';
+        window['answer_' + id] = true;
+        newClass = thisElement.className.replace( 'right', 'down' );
+        thisElement.className = newClass;
+        var expandInterval = setInterval( function() {
+            var h = e.offsetHeight;
+            var sh = e.scrollHeight;
+            if ( h < sh ) {
+                h += 20;
+            }
+            else {
+                clearInterval( expandInterval );
+                window['answer_' + id] = false;
+            }
+            e.style.height = h + "px";
+        }, 8 );
+    }
+    else {
+        window['answer_' + id] = true;
+        newClass = thisElement.className.replace( 'down', 'right' );
+        thisElement.className = newClass;
+        var retractInterval = setInterval( function() {
+            var h = e.offsetHeight;
+            if ( h > 0 ) {
+                h -= 20;
+            }
+            else {
+                e.style.height = "0px";
+                clearInterval( retractInterval );
+                window['answer_' + id] = false;
+            }
+            e.style.height = h + "px";
+        }, 8 );
+    }
+    return false;
 }
