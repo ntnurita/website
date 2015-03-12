f53b23cfa72d180c0f39bc2fd045ea94c415d67b
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2015-03-12 15:59:49 -0600

    reformat js and html in expandable-text and ToubleshootingMacPanel to have 2 space indents

--- root/js/expandable-text-v1.js
+++ root/js/expandable-text-v1.js
@@ -6,58 +6,58 @@
  * @returns {boolean}
  */
 function toggleMe( id ) {
-    var e = document.getElementById( id );
-    var thisElement = document.getElementById( id + '-header' );
-    var newClass;
-    if ( !e || window['answer_' + id] ) {
-        return true;
-    }
-    if ( !e.style.height || e.style.height === "0px" ) {
-        window.location.hash = '';
-        window.location.hash = '#' + id + '-header';
-        window['answer_' + id] = true;
-        newClass = thisElement.className.replace( 'right', 'down' );
-        thisElement.className = newClass;
-        var expandInterval = setInterval( function() {
-            var h = e.offsetHeight;
-            var sh = e.scrollHeight;
-            if ( h < sh ) {
-                h += 20;
-            }
-            else {
-                clearInterval( expandInterval );
-                window['answer_' + id] = false;
-            }
-            e.style.height = h + "px";
-        }, 8 );
-    }
-    else {
-        window['answer_' + id] = true;
-        newClass = thisElement.className.replace( 'down', 'right' );
-        thisElement.className = newClass;
-        var retractInterval = setInterval( function() {
-            var h = e.offsetHeight;
-            if ( h > 0 ) {
-                h -= 20;
-            }
-            else {
-                e.style.height = "0px";
-                clearInterval( retractInterval );
-                window['answer_' + id] = false;
-            }
-            e.style.height = h + "px";
-        }, 8 );
-    }
-    return false;
+  var e = document.getElementById( id );
+  var thisElement = document.getElementById( id + '-header' );
+  var newClass;
+  if ( !e || window[ 'answer_' + id ] ) {
+    return true;
+  }
+  if ( !e.style.height || e.style.height === "0px" ) {
+    window.location.hash = '';
+    window.location.hash = '#' + id + '-header';
+    window[ 'answer_' + id ] = true;
+    newClass = thisElement.className.replace( 'right', 'down' );
+    thisElement.className = newClass;
+    var expandInterval = setInterval( function() {
+      var h = e.offsetHeight;
+      var sh = e.scrollHeight;
+      if ( h < sh ) {
+        h += 20;
+      }
+      else {
+        clearInterval( expandInterval );
+        window[ 'answer_' + id ] = false;
+      }
+      e.style.height = h + "px";
+    }, 8 );
+  }
+  else {
+    window[ 'answer_' + id ] = true;
+    newClass = thisElement.className.replace( 'down', 'right' );
+    thisElement.className = newClass;
+    var retractInterval = setInterval( function() {
+      var h = e.offsetHeight;
+      if ( h > 0 ) {
+        h -= 20;
+      }
+      else {
+        e.style.height = "0px";
+        clearInterval( retractInterval );
+        window[ 'answer_' + id ] = false;
+      }
+      e.style.height = h + "px";
+    }, 8 );
+  }
+  return false;
 }
 
 (function() {
-    var interval = setInterval( function() {
-        if ( typeof $ !== 'undefined' ) {
-            $( document ).ready( function() {
-                toggleMe( window.location.hash.replace( '#', '' ).replace( '-header', '' ) );
-            } );
-            clearInterval( interval );
-        }
-    }, 100 );
+  var interval = setInterval( function() {
+    if ( typeof $ !== 'undefined' ) {
+      $( document ).ready( function() {
+        toggleMe( window.location.hash.replace( '#', '' ).replace( '-header', '' ) );
+      } );
+      clearInterval( interval );
+    }
+  }, 100 );
 })();
