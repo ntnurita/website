d45a6460fe959ca7d3442bda6186fa40326395d0
aaronsamuel137 <aaron.davis.samuel@gmail.com>
2015-04-21 15:06:44 -0600

    add x buttons on sim thumbnails, see #102

--- root/js/contribution-edit.js
+++ root/js/contribution-edit.js
@@ -4,17 +4,14 @@ phet.checkboxToggle = function( simName, checkboxDiv, thumbnailUrl, simType ) {
   var thumbnailId = 'sim-thumbnail-' + simName + '-' + simType;
   var checkbox = checkboxDiv.children[ 0 ];
   var thumbnailTable = document.getElementById( 'sim-thumbnails' );
-  var td = document.getElementById( thumbnailId );
-
-  var currentRow;
+  var container = document.getElementById( thumbnailId );
 
   // if the thumbnail doesn't exist already and the checkbox has just been checked,
   // create a new td (and a new tr if necessary) to hold the thumbnail image and sim badge
-  if ( !td && checkbox.checked ) {
-    //td = document.createElement( 'td' );
-    //td.id = thumbnailId;
+  if ( !container && checkbox.checked ) {
 
-    var container = document.createElement( 'div' );
+    container = document.createElement( 'div' );
+    container.id = thumbnailId;
     container.className = 'sim-thumbnail';
 
     var image = document.createElement( 'img' );
@@ -30,39 +27,12 @@ phet.checkboxToggle = function( simName, checkboxDiv, thumbnailUrl, simType ) {
     container.appendChild( image );
     container.appendChild( badge );
     container.appendChild( x );
-    td.appendChild( container );
-
-    if ( thumbnailTable.children.length ) {
-      currentRow = thumbnailTable.children[ thumbnailTable.children.length - 1 ];
-    }
-    else {
-      currentRow = document.createElement( 'tr' );
-      thumbnailTable.appendChild( currentRow );
-    }
-
-    if ( currentRow.children.length >= 4 ) {
-      var newTr = document.createElement( 'tr' );
-      thumbnailTable.appendChild( newTr );
-      newTr.appendChild( td );
-    }
-    else {
-      currentRow.appendChild( td );
-    }
+    thumbnailTable.appendChild( container );
   }
 
   // if the checkbox has been unchecked, delete the thumbnail and the containing table row if it is empty
-  else if ( td ) {
-    currentRow = td.parentNode;
-    currentRow.removeChild( td );
-    if ( currentRow.children.length === 0 ) {
-      thumbnailTable.removeChild( currentRow );
-    }
-    else {
-      var lastRow = thumbnailTable.children[ thumbnailTable.children.length - 1 ];
-      var lastTd = lastRow.children[ lastRow.children.length - 1 ];
-      lastRow.removeChild( lastTd );
-      currentRow.appendChild( lastTd );
-    }
+  else if ( container ) {
+    thumbnailTable.removeChild( container );
 
     if ( checkbox.checked ) {
       checkbox.checked = false;
